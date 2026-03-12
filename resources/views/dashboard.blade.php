f8116e7ca72960d1cca2a8304a1467f0e1466817
manleticia <manleticia2@gmail.com>
2026-03-12 08:48:13 +0000

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
-        </h2>
+        </h2>fffff
     </x-slot>
 
     <div class="py-12">
